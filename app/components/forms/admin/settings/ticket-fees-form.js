3dd71228a84f2470e5ab66504d3303c1896333b2
vikas.gupta <vikas.gupta.ip@gmail.com>
2018-08-02 08:20:00 +0530

    ticket fee work

--- app/components/forms/admin/settings/ticket-fees-form.js
+++ app/components/forms/admin/settings/ticket-fees-form.js
@@ -1,4 +1,26 @@
 import Component from '@ember/component';
+import { computed } from '@ember/object';
+import { countries } from 'open-event-frontend/utils/dictionary/demography';
+import { paymentCountries, paymentCurrencies } from 'open-event-frontend/utils/dictionary/payment';
+import { orderBy, filter } from 'lodash';
 
 export default Component.extend({
+
+  didInsertElement() {
+    this.get('model').toArray().addObject(this.store.createRecord('ticket-fee'));
+  },
+
+  paymentCountries: computed(function() {
+    return orderBy(filter(countries, country => paymentCountries.includes(country.code)), 'name');
+  }),
+
+  paymentCurrencies: computed(function() {
+    return orderBy(paymentCurrencies, 'name');
+  }),
+
+  actions: {
+    addNewTicket() {
+      this.get('model').toArray().addObject(this.store.createRecord('ticket-fee'));
+    }
+  }
 });
